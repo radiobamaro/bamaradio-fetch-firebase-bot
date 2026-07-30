c6fe2624a744bee3546daa2261fe2bc2b6b184eb
radiobamaro <radiobama.ro@gmail.com>
2026-07-30 23:36:31 +0300

    index.js

--- index.js
+++ index.js
@@ -206,6 +206,6 @@ async function getAlbumArt(song) {
 }
 
 // Verificare din 5 în 5 secunde
-const INTERVAL_SECUNDE = 20;
+const INTERVAL_SECUNDE = 30;
 setInterval(mainRobot, INTERVAL_SECUNDE * 1000);
 mainRobot();
